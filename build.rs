4269c5e1f3b250c01c31cf8cca99e4ebb153b39d
Seng Jik <853974536@qq.com>
2021-09-14 19:26:33 +0800

    submodule opencl sdk

--- build.rs
+++ build.rs
@@ -2,35 +2,6 @@
 use std::{fs::*, process::Command};
 
 fn setup_opencl_sdk() {
-    if !try_exists("./OpenCL-SDK/LICENSE").unwrap() {
-        let status =
-            Command::new("git")
-                .arg("clone")
-                .arg("https://github.com/KhronosGroup/OpenCL-SDK.git")
-                .status()
-                .unwrap();
-                
-        assert!(status.success());
-    }
-
-    let opencl_submodules =
-        try_exists("./OpenCL-SDK/external/OpenCL-ICD-Loader/LICENSE").unwrap()
-        && try_exists("./OpenCL-SDK/external/OpenCL-Headers/LICENSE").unwrap()
-        && try_exists("./OpenCL-SDK/external/OpenCL-CLHPP/LICENSE").unwrap();
-
-    if !opencl_submodules {
-        let status =
-            Command::new("git")
-                .arg("submodule")
-                .arg("update")
-                .arg("--init")
-                .current_dir("./OpenCL-SDK")
-                .status()
-                .unwrap();
-
-        assert!(status.success());
-    }
-
     if !try_exists("./OpenCL-SDK/build").unwrap() {
         create_dir("./OpenCL-SDK/build").unwrap();
     }
